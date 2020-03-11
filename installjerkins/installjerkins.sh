0ac464a49dd2bfa6b5b60fe620854bd9d07469b1
Praveenkumar <42761154+variableEqual2praveen@users.noreply.github.com>
2020-03-11 09:28:19 +0530

    Update installjerkins.sh

--- installjerkins/installjerkins.sh
+++ installjerkins/installjerkins.sh
@@ -1,9 +1,9 @@
-yum install java-1.8.0-openjdk-devel
-yum install curl
-yum install java-1.8.0-openjdk-devel
+yum install java-1.8.0-openjdk-devel -y
+yum install curl -y
+yum install java-1.8.0-openjdk-devel -y
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install jenkins
+yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
 firewall-cmd --permanent --zone=public --add-port=8080/tcp
