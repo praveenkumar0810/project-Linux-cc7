6e04e2d62069d88e88da53857d1d93491170df58
Praveenkumar <42761154+variableEqual2praveen@users.noreply.github.com>
2020-03-11 09:25:32 +0530

    Add files via upload

---
+++ installjerkins/installjerkins.sh
@@ -0,0 +1,11 @@
+yum install java-1.8.0-openjdk-devel
+yum install curl
+yum install java-1.8.0-openjdk-devel
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins
+systemctl start jenkins
+systemctl status jenkins
+firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --reload
+firefox &&
