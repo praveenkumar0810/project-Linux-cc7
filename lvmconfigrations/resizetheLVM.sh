46154f6dea76e74d73d71b6dd0f2457fc8e09a3b
Praveenkumar <42761154+variableEqual2praveen@users.noreply.github.com>
2020-03-14 21:35:34 +0530

    secondcommit"increase the size of the lv "

--- lvmconfigrations/resizetheLVM.sh
+++ lvmconfigrations/resizetheLVM.sh
@@ -1,4 +1,4 @@
-ty#first --> reduce the lv02 size 
+try#first --> reduce the lv02 size 
 # form lv02 (12.5 GB ) to lv02 (6 GB )
 
 #secondly --> increae the lv01 size 
@@ -34,5 +34,41 @@ lsblk #done with the  reduction of lv02 size to 6GB
 lvdisplay
 
 #############################################
+#############################################
 
+# now lets increce the size of the lv01 by balance 6.5 gb
+
+# porcedure
+
+# defrag the vloume lv01
+e4defrag /dev/vg00/lv01 
+
+#unmount the vloume as usual
+umount /dev/vg00/lv01
+
+
+#hash the entry in the fstab
+vim /etc/fstab 
+
+partprobe 
+#by this above command we notify or acknowledge the kernal 
+# that we done some changes in the filesystem
+
+
+df -h
+lsblk 
+
+lvextend -L +6.50G /dev/vg00/lv01
+# in this we are extending the lv first ans then resizing the filesystem
+
+#check the volume using e2fsck 
+e2fsck -f /dev/vg00/lv01
+
+#resize the filesystem size 
+resize2fs /dev/vg00/lv01
+
+lsblk 
+mount /dev/vg00/lv01 /data/lv01
+vim /etc/fstab 
+lsblk 
 
