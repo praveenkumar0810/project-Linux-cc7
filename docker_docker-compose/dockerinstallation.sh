69c304962a4b1290b1dbaa8d1f4ece6392f6471f
Praveenkumar <42761154+variableEqual2praveen@users.noreply.github.com>
2020-04-21 07:16:31 +0530

    Create dockerinstallation.sh

---
+++ docker_docker-compose/dockerinstallation.sh
@@ -0,0 +1,37 @@
+simple setup for docker & dokcer compose 
+
+yum -y install bash-comple*
+
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+yum -y install yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum -y install docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
+systemctl enable docker
+
+
+########auto completion!!!
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+
+
+##docker-compose
+
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+########auto completion!!!
+curl -L https://raw.githubusercontent.com/docker/compose/1.25.5/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
