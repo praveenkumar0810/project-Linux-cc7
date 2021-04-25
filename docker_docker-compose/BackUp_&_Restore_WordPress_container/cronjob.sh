dae4448fae0b7a049a95feb08819e0af62a08f30
Praveenkumar <42761154+praveenkumarv-github@users.noreply.github.com>
2021-04-25 13:17:14 +0530

    Update cronjob.sh

--- docker_docker-compose/BackUp_&_Restore_WordPress_container/cronjob.sh
+++ docker_docker-compose/BackUp_&_Restore_WordPress_container/cronjob.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+d="$(date +"%d-%b-%Y")"
+
+mkdir /backup-drive/manual/"$d"/
+
+
+docker exec db /usr/bin/mysqldump -u root --password='mysqlrootpassword' wordpress > /backup-drive/manual/"$d"/backup.sql
+
+docker exec -it  wordpress tar -cpvzf /opt/wordpress_backup1.tar.gz /var/www/html/
+docker cp wordpress:/opt/wordpress_backup1.tar.gz /backup-drive/manual/"$d"/wordpress_backup1.tar.gz
