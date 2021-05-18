f0227cb1ea6aabc825ddd39958dfafe39c45c4e9
Praveenkumar <42761154+praveenkumarv-github@users.noreply.github.com>
2021-05-18 12:48:43 +0530

    Create 01.sh

---
+++ git/01.sh
@@ -0,0 +1,15 @@
+Configure User and Email
+General Syntax:
+
+git config --global user.name "Your Name"
+git config --global user.email "you@someplace.com"
+Example using course author's information:
+
+git config --global user.name "Jason Taylor"
+git config --global user.email "jason@jasongtaylor.com"
+Listing All Global Configuration Settings
+git config --global --list
+Seeing Git's User-based Config file
+cat ~/.gitconfig
+
+
